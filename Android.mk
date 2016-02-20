150a13ae0b26f7a7284b27596898415391e02370
Daniel Sikorski <dadi.sikorski@gmail.com>
2016-02-20 18:01:16 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter d855,$(TARGET_DEVICE)),)
+ifneq ($(filter geehrc,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
