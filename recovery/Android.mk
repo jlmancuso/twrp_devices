9ec11a8aba7873fdc30fa1500b918970dc9020b8
dadi11 <dadi.sikorski@gmail.com>
2016-02-24 19:53:23 +0100

    init recovery.g3.rc

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -8,3 +8,12 @@ LOCAL_MODULE_CLASS      := RECOVERY_EXECUTABLES
 LOCAL_SRC_FILES         := recovery/init.recovery.touchtowake.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# G3
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.recovery.g3.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := RECOVERY_EXECUTABLES
+LOCAL_SRC_FILES         := recovery/init.recovery.g3.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
