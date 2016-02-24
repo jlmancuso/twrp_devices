d62fb7997ec45025ace47aeeedbf7de81fbe9f26
dadi11 <dadi.sikorski@gmail.com>
2016-02-24 13:36:00 +0100

    build kernel inline

--- omni_d855.mk
+++ omni_d855.mk
@@ -30,11 +30,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
-PRODUCT_COPY_FILES += \
-    device/lge/d855/kernel:kernel \
-    device/lge/d855/dtb:dtb \
-    device/lge/d855/img_info:img_info
-
 PRODUCT_COPY_FILES += device/lge/d855/fstab.g3:recovery/root/fstab.g3
 PRODUCT_COPY_FILES += device/lge/d855/twrp.fstab:recovery/root/etc/twrp.fstab
 
