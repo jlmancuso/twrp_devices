b2ba26184e174cea8a880647ad4bc2b0a74689c5
dadi11 <dadi.sikorski@gmail.com>
2016-04-11 18:21:30 +0200

    (Hopefully) fix the encryption
    bring in keymaster

--- recovery/twrp.mk
+++ recovery/twrp.mk
@@ -21,3 +21,24 @@ PRODUCT_COPY_FILES += \
 # Touch To Wake
 PRODUCT_PACKAGES += \
     init.recovery.touchtowake.rc
+
+# Keymaster
+    $(shell mkdir -p out/target/product/$(PRODUCT_DEVICE)/recovery/root/vendor/firmware/keymaster)
+    $(shell mkdir -p out/target/product/$(PRODUCT_DEVICE)/recovery/root/vendor/lib)
+    $(shell mkdir -p out/target/product/$(PRODUCT_DEVICE)/recovery/root/vendor/lib/hw)
+    $(shell mkdir -p out/target/product/$(PRODUCT_DEVICE)/recovery/root/sbin)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/root/sbin/libdiag.so:recovery/root/sbin/libdiag.so \
+    $(LOCAL_PATH)/root/sbin/libdrmfs.so:recovery/root/sbin/libdrmfs.so \
+    $(LOCAL_PATH)/root/sbin/libdrmtime.so:recovery/root/sbin/libdrmtime.so \
+    $(LOCAL_PATH)/root/sbin/librpmb.so:recovery/root/sbin/librpmb.so \
+    $(LOCAL_PATH)/root/sbin/libssd.so:recovery/root/sbin/libssd.so \
+    $(LOCAL_PATH)/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
+    $(LOCAL_PATH)/root/vendor/firmware/keymaste.b00:recovery/root/vendor/firmware/keymaster/keymaste.b00 \
+    $(LOCAL_PATH)/root/vendor/firmware/keymaste.b01:recovery/root/vendor/firmware/keymaster/keymaste.b01 \
+    $(LOCAL_PATH)/root/vendor/firmware/keymaste.b02:recovery/root/vendor/firmware/keymaster/keymaste.b02 \
+    $(LOCAL_PATH)/root/vendor/firmware/keymaste.b03:recovery/root/vendor/firmware/keymaster/keymaste.b03 \
+    $(LOCAL_PATH)/root/vendor/firmware/keymaste.mdt:recovery/root/vendor/firmware/keymaster/keymaste.mdt \
+    $(LOCAL_PATH)/root/vendor/lib/libQSEEComAPI.so:recovery/root/vendor/lib/libQSEEComAPI.so \
+    $(LOCAL_PATH)/root/vendor/lib/hw/keystore.msm8974.so:recovery/root/vendor/lib/hw/keystore.msm8974.so
