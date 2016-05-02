1a85a79d7cd277094f11d3f0b0ef15a4fac93001
Daniel Sikorski <dadi.sikorski@gmail.com>
2016-05-02 12:25:14 +0200

    fix path

--- omni_d851.mk
+++ omni_d851.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # inherit twrp configs
-$(call inherit-product, device/lge/d855/recovery/twrp.mk)
+$(call inherit-product, device/lge/d851/recovery/twrp.mk)
 
 PRODUCT_NAME := omni_d851
 PRODUCT_DEVICE := d851
